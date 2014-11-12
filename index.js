3643342595f44aaaaedfa7e4ea474d6e8e186f96
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-11-12 13:42:59 +0100

    set script to run every 24HRS i.e daily

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 'use strict';
 
+var reminder = require('./libs/reminder.js');
+var smsClient = require('./libs/sms-client.js');
+var logger = require('./libs/logger.js');
+console.log(logger);
 
+var DAILY_INTERVAL = 86400000;//24 hours in milliseconds
+var reminderMsg = reminder.getReminderMsg();
 
+function sendPendingReminders(){
+  reminder.getPending(new Date())
+    .then(function(res){
+      res.forEach(function(row){
+        if(!row.phoneNo || row.phoneNo.length === 0){
+          logger.warn('Phone No not available for Facility: '+row.facilityId);
+          return;
+        }
+        smsClient.send(row.phoneNo, reminderMsg)
+          .then(function(res){
+            logger.info('Reminder sent to: ' + row.facilityId);
+            //TODO: check if a reminder has been sent for the given period.
+          })
+          .catch(function(err){
+            var errMsg = [
+              'Reminder Failed, Recipient:', row.facilityId,
+              'Reason: ', JSON.stringify(err)
+            ].join(' ');
+            logger.error(errMsg);
+          })
+      });
+    })
+    .catch(function(err) {
+      logger.error(err);
+    });
+}
 
+function main(){
+  setInterval(sendPendingReminders, DAILY_INTERVAL);
+}
+
+main();
